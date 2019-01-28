3e00da98063d48ddac003e80ea3b561e0fe72261
Rishi74744 <rm2998@gmail.com>
2019-01-28 21:15:46 +0530

    New Architecture Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { Router, Params } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from './core/auth.service'
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+@Component( {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+} )
 export class AppComponent {
-  title = 'T';
-
- 
+    title = 'GreenLux';
 }
---
+++ src/app/auth/components/index.ts
@@ -0,0 +1,13 @@
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { UserComponent } from "./user/user.component";
+
+export { LoginComponent } from "./login/login.component";
+export { RegisterComponent } from "./register/register.component";
+export { UserComponent } from "./user/user.component";
+
+export const AUTH_COMPONENT_LIST: Array<any> = [
+    LoginComponent,
+    RegisterComponent,
+    UserComponent
+];
